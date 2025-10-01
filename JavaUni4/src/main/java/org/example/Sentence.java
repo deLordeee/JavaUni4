a54682783da69d51219f0a6e1a3dc7d8d8859eb3
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-10-01 18:06:55 +0300

    Update Sentence.java

--- JavaUni4/src/main/java/org/example/Sentence.java
+++ JavaUni4/src/main/java/org/example/Sentence.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 /**
  * Клас для представлення речення,
- * яке складається зі слів та розділових знаків.
+ * яке складається зі {@link Word} та {@link Punctuation}.
  */
 public class Sentence {
     private final List<Word> words = new ArrayList<>();
-    private final List<Punctuation> punctuations= new ArrayList<>();
+    private final List<Punctuation> punctuations = new ArrayList<>();
 
     /**
      * Створює речення з рядка.
+     * <p>
+     * Речення розбивається на слова та розділові знаки.
+     * </p>
+     *
      * @param sentence текст речення
      */
     public Sentence(String sentence) {
@@ -31,6 +35,11 @@ public class Sentence {
         }
     }
 
+    /**
+     * Повертає список слів у реченні.
+     *
+     * @return список слів
+     */
     public List<Word> getWords() {
         return words;
     }
