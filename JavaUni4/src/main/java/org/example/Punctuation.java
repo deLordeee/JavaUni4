2a30891331e47b3a79c6663ae943afed1c420533
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-10-01 18:06:42 +0300

    Update Punctuation.java

--- JavaUni4/src/main/java/org/example/Punctuation.java
+++ JavaUni4/src/main/java/org/example/Punctuation.java
@@ -2,18 +2,25 @@ package org.example;
 
 /**
  * Клас для представлення розділового знака.
+ * Використовується у класі {@link Sentence}.
  */
 public class Punctuation {
     private final char mark;
 
     /**
      * Конструктор.
+     *
      * @param mark символ розділового знака
      */
     public Punctuation(char mark) {
         this.mark = mark;
     }
 
+    /**
+     * Повертає символ розділового знака.
+     *
+     * @return розділовий знак
+     */
     public char getMark() {
         return mark;
     }
